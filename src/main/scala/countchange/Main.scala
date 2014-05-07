422d5c6554b45ab8f5f06e9669e03be690ea4aa2
fede <fede.lopez@gmail.com>
2014-05-07 21:53:20 +1000

    Added Scala implementation (small refactor)

--- src/main/scala/countchange/Main.scala
+++ src/main/scala/countchange/Main.scala
@@ -7,23 +7,21 @@ object Main {
 
   def countChange(amount: Int, coins: List[Int]): Int = {
 
-    def doCountChange(result: Int, coin: Int, currentAmount: Int, coins: List[Int]): Int = {
+    def doCountChange(coin: Int, currentAmount: Int, coins: List[Int]): Int = {
       var total = 0
       if (currentAmount == amount) total = 1
-      else if (currentAmount + coin <= amount) {
-        for (currentCoin <- coins) {
-          val outstanding = currentCoin + currentAmount
-          total += doCountChange(total, currentCoin, outstanding, coins.filter((aCoin) => currentCoin >= coin))
-        }
+      else if (coin + currentAmount <= amount) {
+        coins.foreach((currentCoin) => {
+          total += doCountChange(currentCoin, currentCoin + currentAmount, coins.filter((aCoin) => currentCoin >= coin))
+        })
       }
-      if (result == amount) print(s"Result found $result")
       total
     }
 
     val goodCoins = coins.filter((coin) => coin > 0).sorted
     if (goodCoins.isEmpty) 0
     else {
-      doCountChange(0, goodCoins.head, 0, goodCoins)
+      doCountChange(goodCoins.head, 0, goodCoins)
     }
   }
 }

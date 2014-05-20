54a368940a9990318ef0539504023f6d3ff4a6f0
fede <fede.lopez@gmail.com>
2014-05-20 22:51:24 +1000

    mostRetweeted done

---
+++ src/test/scala/week3/assignment/TweetSetSuite.scala
@@ -0,0 +1,118 @@
+package week3.assignment
+
+import org.scalatest.FunSuite
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class TweetSetSuite extends FunSuite {
+
+  trait TestSets {
+    val a = new Tweet("a", "a body", 20)
+    val b = new Tweet("b", "b body", 20)
+    val c = new Tweet("c", "c body", 7)
+    val d = new Tweet("d", "d body", 9)
+
+    val set1 = new Empty
+    val set2 = set1.incl(a)
+    val set3 = set2.incl(b)
+    val set4c = set3.incl(c)
+    val set4d = set3.incl(d)
+    val set5 = set4c.incl(d)
+  }
+
+  def asSet(tweets: TweetSet): Set[Tweet] = {
+    var res = Set[Tweet]()
+    tweets.foreach(res += _)
+    res
+  }
+
+  def size(set: TweetSet): Int = asSet(set).size
+
+  test("filter: on empty set") {
+    new TestSets {
+      assert(size(set1.filter(tw => tw.user == "a")) === 0)
+    }
+  }
+
+  test("filter: a on set5") {
+    new TestSets {
+      val result = set5.filter(tw => tw.user == "a")
+      assert(size(result) === 1)
+      assert(result.contains(a))
+    }
+  }
+
+  test("filter: > 8 on set5") {
+    new TestSets {
+      val result = set5.filter(tw => tw.retweets > 8)
+      assert(size(result) === 3)
+      assert(result.contains(a))
+      assert(result.contains(b))
+      assert(result.contains(d))
+    }
+  }
+
+  test("union: set4c and set4d") {
+    new TestSets {
+      assert(size(set4c.union(set4d)) === 4)
+    }
+  }
+
+  test("union: with empty set (1)") {
+    new TestSets {
+      assert(size(set5.union(set1)) === 4)
+    }
+  }
+
+  test("union: with empty set (2)") {
+    new TestSets {
+      assert(size(set1.union(set5)) === 4)
+    }
+  }
+
+  test("union: with set4c") {
+    new TestSets {
+      val e = new Tweet("e", "e body", 12)
+      val f = new Tweet("f", "f body", 15)
+
+      val set6 = new Empty
+      val set7 = set6.incl(e)
+      val set8 = set7.incl(f)
+      val set9 = set8.incl(a)
+
+      val res = set4c.union(set9)
+
+      assert(size(res) === 5)
+      assert(res.contains(e))
+      assert(res.contains(f))
+      assert(res.contains(a))
+      assert(res.contains(b))
+      assert(res.contains(c))
+    }
+  }
+
+  test("descending: set5") {
+    new TestSets {
+      val trends = set5.descendingByRetweet
+      assert(!trends.isEmpty)
+      assert(trends.head.user == "a" || trends.head.user == "b")
+    }
+  }
+
+  test("mostRetweeted: empty") {
+    new TestSets {
+      intercept[NoSuchElementException] {
+        set1.mostRetweeted
+      }
+    }
+  }
+
+  test("mostRetweeted: set5") {
+    new TestSets {
+      val result = set5.mostRetweeted
+      assert(result === a)
+    }
+  }
+}
